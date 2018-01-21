2b239dd16fab9153d3cbb4d4c4c80ff215539430
yiyuezhuo <616271835@163.com>
2018-01-21 23:40:24 +0800

    update

--- statistic_parameter2.py
+++ statistic_parameter2.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-Created on Thu Jan 18 08:12:24 2018
+Created on Sun Jan 21 23:14:00 2018
 
 @author: yiyuezhuo
 """
@@ -13,22 +13,24 @@ from matplotlib.patches import ConnectionPatch
 
 import scipy.stats as stats
 
+from functools import reduce
+
     
 class MultiDistribution(object):
     def __init__(self,expect):
         self.expect = expect
-        self.norm_dis = stats.norm(loc=expect,scale=1)
-        self.norm_2_dis = stats.norm(loc=expect,scale=2)
-        self.lognorm_dis = stats.lognorm(np.sqrt(2*np.log(2)),loc = expect-2)
-        self.chi2_dis = stats.chi2(expect)
-        self.exp_dis = stats.expon(scale=expect)
+        self.norm = stats.norm(loc=expect,scale=1)
+        self.norm_2 = stats.norm(loc=expect,scale=2)
+        self.lognorm = stats.lognorm(np.sqrt(2*np.log(2)),loc = expect-2)
+        self.chi2 = stats.chi2(expect)
+        self.exp = stats.expon(scale=expect)
     def excited(self):
         print('excited!')
 
 delta = 0.01
 
 left = -1
-right = 1000
+right = 100
 sample_size = 1000
 #base_size = 6
 
@@ -39,22 +41,37 @@ graph_right = 6
 
 im_list = []
 
-for expect in np.linspace(2,3,60):
+frame = 60
+frame = 6
+
+for expect in np.linspace(2,3,frame):
 #for expect in [1,1.3,1.6,2]:
-    len_axs = 4
+    print('expect:{} 2->3 60'.format(expect))
+    len_axs = 5
     
     dis = MultiDistribution(expect)
     
     x = np.linspace(left,right,sample_size)
+    step = x[1] - x[0]
     #step = 0.05
     #x = np.arange(left,right,step)
     
-    fig, axs = plt.subplots(len_axs, 1, figsize=(6, 8))
-    axs[0].plot(x,dis.norm_dis.pdf(x))
-    #axs[1].plot(x,norm_2_dis.pdf(x))
-    axs[1].plot(x,dis.lognorm_dis.pdf(x))
-    axs[2].plot(x,dis.chi2_dis.pdf(x))
-    axs[3].plot(x,dis.exp_dis.pdf(x))
+    fig, axs = plt.subplots(len_axs, 1, figsize=(8, 8))
+    axs[0].plot(x,dis.norm.pdf(x))
+    #axs[1].plot(x,norm_2.pdf(x))
+    axs[1].plot(x,dis.lognorm.pdf(x))
+    axs[2].plot(x,dis.chi2.pdf(x))
+    axs[3].plot(x,dis.exp.pdf(x))
+    
+    pdf_list = [dis.norm.pdf(x)*step,
+                dis.lognorm.pdf(x)*step,
+                dis.chi2.pdf(x)*step,
+                dis.exp.pdf(x)*step]
+    sum_pdf = reduce(np.convolve, pdf_list)
+    x2 = np.linspace(left*len(pdf_list),
+                     right*len(pdf_list),
+                     len(sum_pdf)) / len(pdf_list)
+    axs[4].plot(x2,sum_pdf/step)
     
     coordsA = "data"
     coordsB = "data"
@@ -66,7 +83,9 @@ for expect in np.linspace(2,3,60):
     for ax in axs:
         ax.set_xlim(graph_left,graph_right)
         ax.set_ylim(graph_down,graph_up)
-
+        
+    #ax.set_xlim(graph_left,graph_right)
+    #ax.set_ylim(graph_down,graph_up*2)
     
     im_list.append(take_current_image())
     plt.clf()
--- test3.py
+++ test3.py
@@ -9,6 +9,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.patches import ConnectionPatch
 
+from functools import reduce
 
 def f(t):
     return np.exp(-t) * np.cos(2*np.pi*t)
@@ -29,7 +30,7 @@ plt.show()
 import scipy.stats as stats
 
 
-expect = 1
+expect = 2
 
 #x = np.linspace(0.1,4.0,100)
 
@@ -39,9 +40,12 @@ lognorm_dis = stats.lognorm(np.sqrt(2*np.log(2)),loc = expect-2)
 chi2_dis = stats.chi2(expect)
 exp_dis = stats.expon(scale=expect)
 
-fig, axs = plt.subplots(5, 1, figsize=(6, 9))
 
-x = np.linspace(-2,4.0,100)
+#step = 0.05
+#x = np.arange(-1,6.0,step)
+x = np.linspace(-1,6,200)
+
+fig, axs = plt.subplots(5, 1, figsize=(6, 9))
 
 
 axs[0].plot(x,norm_dis.pdf(x))
@@ -53,12 +57,16 @@ axs[4].plot(x,exp_dis.pdf(x))
 xy = (1.0, 0.0)
 coordsA = "data"
 coordsB = "data"
-con = ConnectionPatch(xyA=(1.0,0.0), xyB=(1.0,0.99  ), coordsA=coordsA, coordsB=coordsB,
+con = ConnectionPatch(xyA=(expect,0.0), xyB=(expect,0.99  ), coordsA=coordsA, coordsB=coordsB,
                       axesA=axs[4], axesB=axs[0], shrinkB=5)
 #ax1.add_artist(con)
 axs[4].add_artist(con)
 
 for ax in axs:
-    ax.set_xlim(-2,4)
+    ax.set_xlim(-1,6)
     ax.set_ylim(-0.01,1)
 
+pdf_list = [norm_dis.pdf(x),lognorm_dis.pdf(x),chi2_dis.pdf(x),exp_dis.pdf(x)]
+sum_pdf = reduce(np.convolve, pdf_list)
+
+x2 = np.linspace(-3,24,100*4-3) 
